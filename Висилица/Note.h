f92614a1e59d348c7f71ac8c1f71e7450e69b94e
Евгения Пузырева <satan.eva@mail.ru>
2022-12-26 16:03:46 +0500

    Код готов. Осталось оформить красивый вывод сообщений.

--- Висилица/Note.h
+++ Висилица/Note.h
@@ -1,38 +1,27 @@
 #pragma once
 #include <ctime>
-#include <iostream>
-#include "DateTime.h"
 class Note
 {
 public:
-	Note(DateTime time, std::string text);
+	Note(std::string text);
 
 	~Note();
-	std::string GetTime();
-	void SetTime(int year, int month, int day, int hour, int minute);
 	std::string GetText();
 	void SetText(std::string text);
 
 private:
-	DateTime time;
 	std::string text;
 };
 
-Note::Note(DateTime time, std::string text)
+Note::Note(std::string text)
 {
-	this->time = time;
 	this->text = text;
 }
 
 Note::~Note()
 {
 }
-std::string Note::GetTime() {
-	return this->time.GetTime();
-}
-void Note::SetTime(int year, int month, int day, int hour, int minute) {
-	this->time.SetTime(year, month, day, hour, minute);
-}
+
 std::string Note::GetText() {
 	return this->text;
 }
